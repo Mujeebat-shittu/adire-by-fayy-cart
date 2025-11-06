1ac95befea17610313cd4cf6fd05891247fe4fdd
Mujeebat Shittu <shittuhoney@gmail.com>
2025-11-06 22:58:56 +0100

    responsiveness

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -60,7 +60,7 @@ function Product() {
                 <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 place-content-center gap-y-8 gap-x-6 mx-auto my-10 space-y-4 space-x-4 ">
                     {filteredProducts?.map((product) => (
                         <Card key={product.id}
-                            className="max-w-[350px] md:w-[260px] lg:w-[300px] bg-[#d1d9ce] dark:bg-[#809679] dark:text-[#1a1a1a] py-6 px-8 flex flex-col text-left hover:scale-[1.07] hover:translate-y-3 cursor-pointer ring-2 ring-[#d0d8cd]/50 hover:ring-[#d0d8cd]/80 transition-all duration-300"
+                            className="max-w-[350px] md:w-[260px] lg:w-[300px] bg-[#d1d9ce] dark:bg-[#809679] dark:text-[#1a1a1a] py-6 px-8 flex flex-col items-center justify-center text-left hover:scale-[1.07] hover:translate-y-3 cursor-pointer ring-2 ring-[#d0d8cd]/50 hover:ring-[#d0d8cd]/80 transition-all duration-300"
                         >
                             <CardContent className="text-left">
                                 <img src={product.image} alt="" className="h-[270px] w-auto mx-auto" />
