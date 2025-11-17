a9455d908da187ba3b4c88641f0ff42deb8456c2
Mujeebat Shittu <shittuhoney@gmail.com>
2025-11-17 19:51:57 +0100

    typo

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -6,7 +6,6 @@ import { Dot } from "lucide-react";
 function About() {
     return (
         <>
-        <div className="overflow-hidden">
             <Header />
             <div className="bg-linear-to-r from-white to-[#d1d9ce] dark:bg-linear-to-r dark:from-[#1a1a1a] dark:to-[#809679] text-black flex min-h-screen items-center justify-center flex-col gap-2">
                 <div className="flex absolute flex-col max-w-[350px] sm:max-w-[400px] md:max-w-[500px] h-[600px] sm:h-[500px] items-center justify-center shadow-2xl bg-[#d0d8cd] dark:bg-[#809679] text-[#1a1a1a] rounded-lg">             
@@ -29,7 +28,6 @@ function About() {
                     <img src={BrandBox} alt="" className="w-20 h-20 sm:w-30 sm:h-30 absolute right-0 bottom-0 rounded-lg" />
                 </div>
             </div>
-        </div>
             
         </>
     )
