a8d4dd4904e13a37363bde8faa3903f7e8bdb45a
Mujeebat Shittu <shittuhoney@gmail.com>
2025-11-18 02:31:22 +0100

    resolving conflicts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-<<<<<<< HEAD
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
@@ -16,17 +15,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
 
-=======
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
->>>>>>> a9455d908da187ba3b4c88641f0ff42deb8456c2
   },
 })
